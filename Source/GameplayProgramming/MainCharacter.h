ea347223a904cf6f6b62070e7050634324d55095
William <50932749+BilliamsFluster@users.noreply.github.com>
2023-05-12 02:42:58 -0400

    Character movement and classes
    Classes added: Character, controller, Gamemode
    
    BP classes children added: Character, controller, gamemode
    
    Character movement added

---
+++ Source/GameplayProgramming/MainCharacter.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+
+UCLASS()
+class GAMEPLAYPROGRAMMING_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMainCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Handles input for moving forward and backward.
+	UFUNCTION()
+	void MoveForward(float Value);
+
+	// Handles input for moving right and left.
+	UFUNCTION()
+	void MoveRight(float Value);
+
+	void TurnAtRate(float Rate);
+
+	void LookUpAtRate(float Rate);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+
+private:
+	// Camera boom positioning the camera behind the character.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		USpringArmComponent* CameraBoom;
+
+	// Camera that follows the character.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		UCameraComponent* FollowCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+		float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+		float BaseLookUpRate;
+
+};
